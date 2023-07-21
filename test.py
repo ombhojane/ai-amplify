34f518b9267521f69f7801eff2da71abff6d501a
prathzsthakare <prathzsthakare09@gmail.com>
2023-07-21 23:56:59 +0530

    wolframaplha bot ai

---
+++ test.py
@@ -0,0 +1,9 @@
+import wolframalpha
+
+client = wolframalpha.Client('JGUTQ4-RG973Q6L5E')
+
+while True :
+    query = str(input('Query:  '))
+    res = client.query(query)
+    output = next(res.results).text
+    print(output)
