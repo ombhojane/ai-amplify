b78420ba579df4bc162ddb5590353b9b662c3e69
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-07-22 06:14:50 +0530

    my profile done

--- Surf/screens/MyProfileScreen.js
+++ Surf/screens/MyProfileScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput, Share, Animated } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 const MyProfile = () => {
   // Sample user data (you can replace this with your own data)
@@ -38,18 +39,23 @@ const MyProfile = () => {
 
   // Function to handle saving the profile changes
   const handleSaveProfile = () => {
+    // Implement your logic for updating the profile here
+    // For this example, we'll update the user state with the edited values.
     setUser({
       ...user,
       name: editedName,
       bio: editedBio,
     });
     setIsEditMode(false); // Switch back to "Edit Profile" mode after saving
+    // You can also send the updated data to a backend API for permanent storage.
   };
 
   // Function to handle sharing the profile
   const handleShareProfile = () => {
     // Implement your logic for sharing the profile here
+    // For this example, we'll use the Share API provided by React Native.
 
+    // The content you want to share, here we'll combine the user's name and bio.
     const contentToShare = `Check out ${user.name}'s profile: ${user.bio}`;
 
     Share.share({
@@ -71,7 +77,7 @@ const MyProfile = () => {
   }, []);
 
   return (
-    <Animated.View style={[styles.container, { opacity: fadeInAnimation }]}>
+    <View style={styles.container}>
       <View style={styles.profileContainer}>
         {/* Profile Picture */}
         <View style={styles.avatarContainer}>
@@ -105,52 +111,6 @@ const MyProfile = () => {
             </>
           )}
 
-          {/* Emotion Icons and Labels */}
-          <View style={styles.emotionContainer}>
-            <View style={styles.emotionItem}>
-              <Icon name="angry" size={30} color="#FF3333" />
-              <Text style={styles.emotionLabel}>Angry</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.angry}%</Text>
-              <Text style={styles.emotionIcon}>😡</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="meh-o" size={30} color="#FFA500" />
-              <Text style={styles.emotionLabel}>Dis</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.dis}%</Text>
-              <Text style={styles.emotionIcon}>😐</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="frown-o" size={30} color="#FF6600" />
-              <Text style={styles.emotionLabel}>Fear</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.fear}%</Text>
-              <Text style={styles.emotionIcon}>😨</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="smile-o" size={30} color="#FFCC33" />
-              <Text style={styles.emotionLabel}>Happy</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.happy}%</Text>
-              <Text style={styles.emotionIcon}>😄</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="meh-o" size={30} color="#6699CC" />
-              <Text style={styles.emotionLabel}>Neutral</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.neutral}%</Text>
-              <Text style={styles.emotionIcon}>😐</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="frown-o" size={30} color="#003366" />
-              <Text style={styles.emotionLabel}>Sad</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.sad}%</Text>
-              <Text style={styles.emotionIcon}>😢</Text>
-            </View>
-            <View style={styles.emotionItem}>
-              <Icon name="smile-o" size={30} color="#66CC99" />
-              <Text style={styles.emotionLabel}>Surprise</Text>
-              <Text style={styles.emotionPercentage}>{emotionData.surprise}%</Text>
-              <Text style={styles.emotionIcon}>😲</Text>
-            </View>
-          </View>
-
           {/* Edit and Share Buttons */}
           <View style={styles.buttonsContainer}>
             {/* Edit/Save Button */}
@@ -168,15 +128,61 @@ const MyProfile = () => {
           </View>
         </View>
       </View>
-    </Animated.View>
+
+      {/* Emotion Icons and Labels */}
+      <View style={styles.emotionContainer}>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😠</Text>
+          <Text style={styles.emotionLabel}>Angry</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.angry}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😞</Text>
+          <Text style={styles.emotionLabel}>Dis</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.dis}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😨</Text>
+          <Text style={styles.emotionLabel}>Fear</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.fear}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😄</Text>
+          <Text style={styles.emotionLabel}>Happy</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.happy}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😐</Text>
+          <Text style={styles.emotionLabel}>Neutral</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.neutral}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😢</Text>
+          <Text style={styles.emotionLabel}>Sad</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.sad}%</Text>
+        </View>
+        <View style={styles.emotionItem}>
+          <Text style={styles.emotionIcon}>😲</Text>
+          <Text style={styles.emotionLabel}>Surprise</Text>
+          <Text style={styles.emotionPercentage}>{emotionData.surprise}%</Text>
+        </View>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  // Styles for the profile component
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent', // Set to transparent, as we will use a gradient background instead
+  },
+  gradientBackground: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   profileContainer: {
     flexDirection: 'row',
@@ -193,7 +199,7 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 50,
     borderWidth: 3,
-    borderColor: '#fff',
+    borderColor: '#000',
   },
   profileInfoContainer: {
     flex: 1,
@@ -201,10 +207,11 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#000',
   },
   bio: {
     fontSize: 16,
-    color: '#888',
+    color: '#000',
     marginBottom: 10,
     marginTop: 10,
   },
@@ -215,6 +222,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#ccc',
     paddingVertical: 5,
+    color: '#000',
   },
   bioInput: {
     fontSize: 16,
@@ -223,6 +231,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#ccc',
     paddingVertical: 5,
+    color: '#000',
   },
   buttonsContainer: {
     flexDirection: 'row',
@@ -236,7 +245,7 @@ const styles = StyleSheet.create({
     borderRadius: 24,
   },
   editSaveButtonText: {
-    color: '#fff',
+    color: '#000',
     fontSize: 16,
     fontWeight: 'bold',
   },
@@ -247,31 +256,33 @@ const styles = StyleSheet.create({
     borderRadius: 24,
   },
   shareButtonText: {
-    color: '#fff',
+    color: '#000',
     fontSize: 16,
     fontWeight: 'bold',
   },
   emotionContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
     marginTop: 20,
   },
   emotionItem: {
     alignItems: 'center',
-    width: '12%', // Adjust the width to control the spacing between emotion items
+    width: '25%',
+    marginBottom: 20,
   },
   emotionLabel: {
     marginTop: 5,
     fontSize: 16,
     fontWeight: 'bold',
+    color: '#000',
   },
   emotionPercentage: {
     fontSize: 14,
-    color: '#888',
+    color: '#000',
   },
   emotionIcon: {
-    fontSize: 20,
+    fontSize: 30,
   },
 });
-
 export default MyProfile;
